53dae755e26bca792da34dd06c012ad493c21ba6
Dhananjay <dhananjayagrawalmain001@gmail.com>
2026-01-10 23:58:07 +0530

    new

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -6,11 +6,11 @@ function ProgressBar() {
   return (
     <div className=" h-[3px] relative z-50 ">
       {/* Track (full progress line) */}
-      <div className="absolute inset-0 bg-gray-800 pr-[40vw] sm:pr-0" />
+      <div className="absolute inset-0 bg-gray-800 pr-[32vw] sm:pr-0" />
 
       {/* Fill (animated progress) */}
       <motion.div
-        className="pr-[40vw] sm:pr-0 absolute inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 origin-left"
+        className="pr-[32vw] sm:pr-0 absolute inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 origin-left"
         style={{ scaleX: scrollYProgress }}
       />
     </div>
