2d9ca3cec76baa4b19bcb253c4b9e1d135916073
Dhananjay <dhananjayagrawalmain001@gmail.com>
2026-03-04 00:02:40 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,73 @@
 import { HashRouter } from "react-router-dom";
 import { About, Contact, Work, Hero, Navbar } from "./components";
+import { useEffect, useRef, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Loader from "./components/Loader";
 import Cursor from "./Cursor";
 
 const App = () => {
+  const [progress, setProgress] = useState(0);
+  const [showLoader, setShowLoader] = useState(true);
+  const startTimeRef = useRef(Date.now());
+
+  // Fake loading progress
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 5;
+      });
+    }, 80);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // Ensure minimum 2 seconds
+  useEffect(() => {
+    if (progress === 100) {
+      const elapsed = Date.now() - startTimeRef.current;
+      const remaining = Math.max(2000 - elapsed, 0);
+
+      const timer = setTimeout(() => {
+        setShowLoader(false);
+      }, remaining);
+
+      return () => clearTimeout(timer);
+    }
+  }, [progress]);
+
   return (
-    <HashRouter>
+    <>
+      <AnimatePresence mode="wait">
+        {showLoader && <Loader progress={progress} />}
+      </AnimatePresence>
 
-      <Cursor />
-      <Navbar />
+      {!showLoader && (
+        <HashRouter>
+          <Cursor />
+          <Navbar />
 
-      <section id="home">
-        <Hero />
-      </section>
+          <section id="home">
+            <Hero />
+          </section>
 
-      <section className="scroll-mt-10" id="work">
-        <Work />
-      </section>
+          <section className="scroll-mt-10" id="work">
+            <Work />
+          </section>
 
-      <section id="about" className="scroll-mt-5">
-        <About />
-      </section>
+          <section id="about" className="scroll-mt-5">
+            <About />
+          </section>
 
-      <section id="contact" className="scroll-mt-0">
-        <Contact />
-      </section>
-    </HashRouter>
+          <section id="contact" className="scroll-mt-0">
+            <Contact />
+          </section>
+        </HashRouter>
+      )}
+    </>
   );
 };
 
